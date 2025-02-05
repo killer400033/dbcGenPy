afee25e10c320ce54025a2b740b4765c70d61cad
Pahan <30065466@westernsydney.edu.au>
2025-02-06 06:25:15 +1100

    feat(setup): Added setup scripts

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
-c#!/bin/bash
-
-# Run git submodule update --init --recursive
-echo "Updating submodules..."
-git submodule update --init --recursive
+#!/bin/bash
 
 # Install cantools using pip
 echo "Installing cantools..."
 python3 -m pip install cantools
+
+# Check if .config already exists
+if [ ! -f "../../.config" ]; then
+    echo ".config does not exist, copying .config.example..."
+    cp .config.example ../../.config
+else
+    echo ".config already exists, skipping copy."
+fi
 
 echo "Done."
