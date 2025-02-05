5d7a8ba37e465c7f1cb34c102144fd8f4d826f09
Pahan G <pahang20031120@gmail.com>
2025-02-05 21:20:17 +0000

    Merged in pahan/setup-basics (pull request #5)
    Pahan/setup basics
    
    * feat(Pack functions): Added pack functions and dbc-files submodule
    
    * fix(Pack functions): Fixed bug with pack functions
    
    * feat(encode functions): Added encode functions for inidivual signals
    
    * fix(sig_type_dec.h): Fixed bug with type decode function prototypes
    
    * merged origin/main
    
    * fix(message duplicated): Fixed bug with generating duplicate code
    
    * fix(duplicate macros): Fixed bug with duplicate #define macros
    
    * fix(duplicate macros): Extra fix to update all macros
    
    * tweak(config.py): gitignored config and added example config
    
    * tweak(config): Changed how config file works
    
    * fix(config.py): Fixed error message when config is missing
    
    * fix(config.py): Fixed route issue with .config
    
    * tweak(Message Duplicates): Added warnings for message duplicates
    
    * feat(setup): Added setup scripts
    
    * feat(setup): Added setup scripts
    
    * fix(genUnits): Added message name to macro name to avoid repetition

--- gen/genUnits.py
+++ gen/genUnits.py
@@ -1,6 +1,6 @@
 def generateUnitCode(message):
     unitcode = ""
     for signal in message.signals:
-        unitcode += f"#define {signal.name.upper()}_UNIT \"{signal.unit}\"\n"
+        unitcode += f"#define {message.name.upper()}_{signal.name.upper()}_UNIT \"{signal.unit}\"\n"
 
     return unitcode
