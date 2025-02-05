e7b1876e0e11e085dd1f7f92cf57ffd7af884279
Pahan <30065466@westernsydney.edu.au>
2025-02-06 03:06:43 +1100

    fix(config.py): Fixed error message when config is missing

--- config.py
+++ config.py
@@ -1,34 +1,39 @@
 import configparser
 import os
+import sys
 
-config_path = ".config"
+config_path = ".cdonfig"
 config = configparser.ConfigParser()
-config.read(config_path)
 
-# Convert values from the config file
-USE_SIGFLOAT = config.getboolean("GEN", "USE_SIGFLOAT")
-SIGFLOAT_TYPE = config.get("GEN", "SIGFLOAT_TYPE")
-FLOAT_LITERAL_PREC = config.getint("GEN", "FLOAT_LITERAL_PREC")
+if os.path.exists(config_path):
+    config.read(config_path)
+    # Convert values from the config file
+    USE_SIGFLOAT = config.getboolean("GEN", "USE_SIGFLOAT")
+    SIGFLOAT_TYPE = config.get("GEN", "SIGFLOAT_TYPE")
+    FLOAT_LITERAL_PREC = config.getint("GEN", "FLOAT_LITERAL_PREC")
 
-# Options
-GENERATE_PACK = config.getboolean("OPTIONS", "GENERATE_PACK")
-GENERATE_UNPACK = config.getboolean("OPTIONS", "GENERATE_UNPACK")
-GENERATE_SIGNAL_UNITS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_UNITS")
-GENERATE_SIGNAL_ENUMS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_ENUMS")
-GENERATE_SIGNAL_VALS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_VALS")
-GENERATE_SIGNAL_TYPE_DECODE = config.getboolean("OPTIONS", "GENERATE_SIGNAL_TYPE_DECODE")
+    # Options
+    GENERATE_PACK = config.getboolean("OPTIONS", "GENERATE_PACK")
+    GENERATE_UNPACK = config.getboolean("OPTIONS", "GENERATE_UNPACK")
+    GENERATE_SIGNAL_UNITS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_UNITS")
+    GENERATE_SIGNAL_ENUMS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_ENUMS")
+    GENERATE_SIGNAL_VALS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_VALS")
+    GENERATE_SIGNAL_TYPE_DECODE = config.getboolean("OPTIONS", "GENERATE_SIGNAL_TYPE_DECODE")
 
-# DBC files (stored as a list)
-DBC_DIRS = [
-    line.strip() 
-    for line in config.get("DBC_FILES", "DBC_DIRS").splitlines() 
-    if line.strip() and not line.strip().startswith("#")
-]
+    # DBC files (stored as a list)
+    DBC_DIRS = [
+        line.strip() 
+        for line in config.get("DBC_FILES", "DBC_DIRS").splitlines() 
+        if line.strip() and not line.strip().startswith("#")
+    ]
 
-# File Dirs
-SOURCE_OUT_DIR = config.get("FILE_DIRS", "SOURCE_OUT_DIR")
-HEADER_OUT_DIR = config.get("FILE_DIRS", "HEADER_OUT_DIR")
-DBC_FILE_DIR = config.get("FILE_DIRS", "DBC_FILE_DIR")
+    # File Dirs
+    SOURCE_OUT_DIR = config.get("FILE_DIRS", "SOURCE_OUT_DIR")
+    HEADER_OUT_DIR = config.get("FILE_DIRS", "HEADER_OUT_DIR")
+    DBC_FILE_DIR = config.get("FILE_DIRS", "DBC_FILE_DIR")
+else:
+    sys.stderr.write(f"Error: The file {config_path} does not exist.\n")
+    sys.exit(1)
 
 # Don't change
 MAIN_NAME="dbc"
