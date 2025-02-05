31ce0fad8149120c94cd363edec19f2cb96f4073
Pahan <30065466@westernsydney.edu.au>
2025-02-06 02:59:55 +1100

    tweak(config): Changed how config file works

---
+++ config.py
@@ -0,0 +1,41 @@
+import configparser
+import os
+
+config_path = ".config"
+config = configparser.ConfigParser()
+config.read(config_path)
+
+# Convert values from the config file
+USE_SIGFLOAT = config.getboolean("GEN", "USE_SIGFLOAT")
+SIGFLOAT_TYPE = config.get("GEN", "SIGFLOAT_TYPE")
+FLOAT_LITERAL_PREC = config.getint("GEN", "FLOAT_LITERAL_PREC")
+
+# Options
+GENERATE_PACK = config.getboolean("OPTIONS", "GENERATE_PACK")
+GENERATE_UNPACK = config.getboolean("OPTIONS", "GENERATE_UNPACK")
+GENERATE_SIGNAL_UNITS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_UNITS")
+GENERATE_SIGNAL_ENUMS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_ENUMS")
+GENERATE_SIGNAL_VALS = config.getboolean("OPTIONS", "GENERATE_SIGNAL_VALS")
+GENERATE_SIGNAL_TYPE_DECODE = config.getboolean("OPTIONS", "GENERATE_SIGNAL_TYPE_DECODE")
+
+# DBC files (stored as a list)
+DBC_DIRS = [
+    line.strip() 
+    for line in config.get("DBC_FILES", "DBC_DIRS").splitlines() 
+    if line.strip() and not line.strip().startswith("#")
+]
+
+# File Dirs
+SOURCE_OUT_DIR = config.get("FILE_DIRS", "SOURCE_OUT_DIR")
+HEADER_OUT_DIR = config.get("FILE_DIRS", "HEADER_OUT_DIR")
+DBC_FILE_DIR = config.get("FILE_DIRS", "DBC_FILE_DIR")
+
+# Don't change
+MAIN_NAME="dbc"
+SIGNAL_VALS_NAME="sig_vals"
+SIGNAL_UNITS_NAME="sig_units"
+SIGNAL_ENUMS_NAME="sig_enum"
+SIGNAL_TYPE_DECODE_NAME="sig_type_dec"
+UNPACK_SCALE_OFFSET_PREFIX="UNPACK_SCALE_OFFSET_"
+PACK_SCALE_OFFSET_PREFIX="PACK_SCALE_OFFSET_"
+BYTE_INIT_VALUE="0u"
